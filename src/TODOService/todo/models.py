851ba8c0be520c6cb755d5bb5a974de8cff1d28b
Diamond Mohanty <memohanty.diamond@gmail.com>
2020-04-28 23:20:39 +0530

    Serializers Added

--- src/TODOService/todo/models.py
+++ src/TODOService/todo/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+
+class TODO(models.Model):
+
+    description = models.CharField(max_length=255, blank=False, null=False)
+    completion_date = models.DateTimeField()
+    status = models.BooleanField(default=False)
